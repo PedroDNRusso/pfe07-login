1083980a461d746d77f832648c155c01169a11b9
Aluno <pedronaddeu@gmail.com>
2025-06-03 08:12:42 -0300

    newrep

---
+++ web/script.js
@@ -0,0 +1,19 @@
+const EMAIL_CORRETO = "meucachorro2023@gmail.com";
+const SENHA_CORRETA = "meupai7568";
+
+function fazerLogin() {
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+    const mensagem = document.getElementById("mensagem");
+
+    if (email === EMAIL_CORRETO && senha === SENHA_CORRETA) {
+        mensagem.style.color = "green";
+        mensagem.textContent = "Login realizado com sucesso!";
+        setTimeout(() => {
+            window.location.href = "home.html"; // Altere para o nome da página desejada
+        }, 1000);
+    } else {
+        mensagem.style.color = "red";
+        mensagem.textContent = "Email ou senha incorretos.";
+    }
+}
